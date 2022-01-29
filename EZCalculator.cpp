552bed920a54e1094483ff125add348bff42c235
Im A Wayne <67802987+WayneLikesMountains@users.noreply.github.com>
2022-01-29 12:13:13 -0800

    Update EZCalculator.cpp
    The starting input and outer loop setup.

--- EZCalculator.cpp
+++ EZCalculator.cpp
@@ -6,4 +6,18 @@ using namespace std;
 int main()
 {
     cout << "Hello Planet" << endl; 
+    
+    // Calculation application
+    int input;
+    float result = 0;
+
+    cout << "Use 1-4 to either: 1 add, 2 subtract, 3 multiply, 4 devide, 0 to quit";
+
+    cin >> input;
+
+    if (input != 0) {
+
+    }
+
+    cout << "Application Ended.." << endl;
 }

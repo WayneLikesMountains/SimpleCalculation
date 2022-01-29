632dc09c2bf478f7ea35b378a9fbf8bfd775bb5c
Im A Wayne <67802987+WayneLikesMountains@users.noreply.github.com>
2022-01-29 12:32:02 -0800

    Update EZCalculator.cpp
    Input test function to make sure the user is entering the correct input.

--- EZCalculator.cpp
+++ EZCalculator.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 using namespace std;
 
+// prototypes
+bool testInput(int inInfo);
+
 int main()
 {
     cout << "Hello Planet" << endl; 
@@ -15,9 +18,25 @@ int main()
 
     cin >> input;
 
+    // test input
+    while (!testInput(input)) {
+        cout << "Incorrect input - - -" << endl;
+        cout << "Use 1-4 to either: 1 add, 2 subtract, 3 multiply, 4 devide, 0 to quit";
+        cin >> input;
+    }    
+    
     if (input != 0) {
 
     }
 
     cout << "Application Ended.." << endl;
+}
+
+bool testInput(int inInfo) {
+    if (inInfo >= 0 || inInfo <= 4) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }

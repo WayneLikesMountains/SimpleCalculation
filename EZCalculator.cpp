3bde11f72376257d2fc6ee1c227517a4eb3b7570
Im A Wayne <67802987+WayneLikesMountains@users.noreply.github.com>
2022-01-29 13:05:22 -0800

    Update EZCalculator.cpp
    Added the program loop with all + - * / operations on 2  user inputted numbers.

--- EZCalculator.cpp
+++ EZCalculator.cpp
@@ -25,8 +25,47 @@ int main()
         cin >> input;
     }    
     
-    if (input != 0) {
+    while (input != 0) {
+        if (input == 1) {
+            cout << "Enter 2 numbers to add as x y" << endl;
+            cin >> num1 >> num2;
+            cin.ignore();
+            result = num1 + num2;
+            cout << "Result is: " << result << endl;
+        }
 
+        if (input == 2) {
+            cout << "Enter 2 numbers to subtract as x y" << endl;
+            cin >> num1 >> num2;
+            cin.ignore();
+            result = num1 - num2;
+            cout << "Result is: " << result << endl;
+        }
+
+        if (input == 3) {
+            cout << "Enter 2 numbers to multiply as x y" << endl;
+            cin >> num1 >> num2;
+            cin.ignore();
+            result = num1 * num2;
+            cout << "Result is: " << result << endl;
+        }
+
+        if (input == 4) {
+            cout << "Enter 2 numbers to devide as x y" << endl;
+            cin >> num1 >> num2;
+            cin.ignore();
+            result = num1 / num2;
+            cout << "Result is: " << result << endl;
+        }
+
+        cout << "Anything else?" << endl;
+        cout << "Use 1-4 to either: 1 add, 2 subtract, 3 multiply, 4 devide, 0 to quit";
+        cin >> input;
+        while (!testInput(input)) {
+            cout << "Incorrect input - - -" << endl;
+            cout << "Use 1-4 to either: 1 add, 2 subtract, 3 multiply, 4 devide, 0 to quit";
+            cin >> input;
+        }
     }
 
     cout << "Application Ended.." << endl;
